2e0ff6a3bab67901e4039daada3c272098aab912
HotTornado <denizberkaykalkan@gmail.com>
2023-11-18 15:59:21 +0300

    update

---
+++ assets/components/head.php
@@ -0,0 +1,6 @@
+<head>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/js/bootstrap.js">
+    <link rel="stylesheet" href="style.scss">
+    <link rel="stylesheet" href="css/style.css">
+</head>
