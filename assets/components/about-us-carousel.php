46e919c7a62f6aac444e28eff0cdfbaf9c5a4e72
HotTornado <denizberkaykalkan@gmail.com>
2023-11-19 21:58:06 +0300

    Update about-us-carousel.php

--- assets/components/about-us-carousel.php
+++ assets/components/about-us-carousel.php
@@ -1,11 +1,11 @@
 <div class="owl-carousel about-us-carousel owl-theme">
     <div class="item">
-      <img src="assets/images/carousel-1.jpg" alt="carousel" class="object-fit-cover carousel-image">
+      <img src="assets/images/carousel-2.jpg" alt="carousel" class="object-fit-cover carousel-image">
     </div>
     <div class="item">
       <img src="assets/images/carousel-2.jpg" alt="carousel" class="object-fit-cover carousel-image">
     </div>
     <div class="item">
-      <img src="assets/images/carousel-3.jpg" alt="carousel" class="object-fit-cover carousel-image">
+      <img src="assets/images/carousel-2.jpg" alt="carousel" class="object-fit-cover carousel-image">
     </div>
 </div>
